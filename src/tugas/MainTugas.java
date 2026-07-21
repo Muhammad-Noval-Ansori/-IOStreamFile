94639f0c660b08db666d7eb01d3a3e3856307609
Muhammad-Noval-Ansori <ancollann@gmail.com>
2026-07-22 04:11:36 +0800

    Update MainTugas.java

--- src/tugas/MainTugas.java
+++ src/tugas/MainTugas.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tugas;
 
-/**
- *
- * @author USER
- */
 public class MainTugas {
-    
+
+    public static void main(String[] args) {
+
+        Gudang gudang = new Gudang();
+
+        gudang.tambahBarang(new Barang("Laptop", 8000000, 5));
+        gudang.tambahBarang(new Barang("Mouse", 150000, 20));
+        gudang.tambahBarang(new Barang("Keyboard", 300000, 10));
+        gudang.tambahBarang(new Barang("Printer", 2500000, 3));
+        gudang.tambahBarang(new Barang("Monitor", 2200000, 6));
+
+        System.out.println("===== DATA BARANG =====");
+        gudang.tampilkanSemua();
+
+        String[] kategori = {
+            "Elektronik",
+            "Aksesoris",
+            "Peralatan"
+        };
+
+        System.out.println("===== KATEGORI =====");
+
+        for (String k : kategori) {
+            System.out.println(k);
+        }
+
+        gudang.simpanKeBerkas();
+
+        Gudang gudangBaru = new Gudang();
+
+        gudangBaru.muatDariBerkas();
+
+        System.out.println("\n===== DATA DARI FILE =====");
+        gudangBaru.tampilkanSemua();
+
+        System.out.println("===== TOTAL NILAI PERSEDIAAN =====");
+        System.out.println("Rp " + gudangBaru.totalNilai());
+    }
 }
