379aa956e7b73c7b7da4825bb6fffe9b7174947c
Muhammad-Noval-Ansori <ancollann@gmail.com>
2026-07-22 04:09:36 +0800

    Update MainKontak.java

--- src/bagian3/kontak/MainKontak.java
+++ src/bagian3/kontak/MainKontak.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package bagian3.kontak;
+package kontak;
 
-/**
- *
- * @author USER
- */
-public class MainKontak {
-    
+public class Main {
+
+    public static void main(String[] args) {
+
+        BukuKontak buku = new BukuKontak();
+
+        buku.tambahKontak(new Kontak("Andi", "0811111111", "andi@gmail.com"));
+        buku.tambahKontak(new Kontak("Budi", "0822222222", "budi@gmail.com"));
+        buku.tambahKontak(new Kontak("Citra", "0833333333", "citra@gmail.com"));
+
+        buku.simpanKeBerkas();
+
+        System.out.println("===== DAFTAR KONTAK =====");
+        buku.tampilkanSemua();
+
+        System.out.println("===== CARI KONTAK =====");
+        buku.cariKontak("Budi");
+
+        System.out.println("===== HAPUS KONTAK =====");
+        buku.hapusKontak("Andi");
+
+        System.out.println("===== SETELAH DIHAPUS =====");
+        buku.tampilkanSemua();
+    }
 }
