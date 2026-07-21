81dce83f23cab7a18df3ba64a59acc5340ada2da
Muhammad-Noval-Ansori <ancollann@gmail.com>
2026-07-22 04:11:06 +0800

    Update Gudang.java

--- src/tugas/Gudang.java
+++ src/tugas/Gudang.java
@@ -1,13 +1,76 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tugas;
 
-/**
- *
- * @author USER
- */
+import java.util.ArrayList;
+import java.io.*;
+
 public class Gudang {
-    
+
+    private ArrayList<Barang> daftarBarang = new ArrayList<>();
+    private final String FILE = "barang.txt";
+
+    public void tambahBarang(Barang barang) {
+        daftarBarang.add(barang);
+    }
+
+    public void tampilkanSemua() {
+        for (Barang b : daftarBarang) {
+            b.info();
+        }
+    }
+
+    public void simpanKeBerkas() {
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(FILE));
+
+            for (Barang b : daftarBarang) {
+                bw.write(b.keBaris());
+                bw.newLine();
+            }
+
+            bw.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void muatDariBerkas() {
+
+        daftarBarang.clear();
+
+        try {
+
+            BufferedReader br = new BufferedReader(new FileReader(FILE));
+
+            String baris;
+
+            while ((baris = br.readLine()) != null) {
+
+                String[] data = baris.split(";");
+
+                daftarBarang.add(new Barang(
+                        data[0],
+                        Double.parseDouble(data[1]),
+                        Integer.parseInt(data[2])
+                ));
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println("File belum ada.");
+        }
+    }
+
+    public double totalNilai() {
+
+        double total = 0;
+
+        for (Barang b : daftarBarang) {
+            total += b.getHarga() * b.getStok();
+        }
+
+        return total;
+    }
 }
