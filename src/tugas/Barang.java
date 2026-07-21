0a23a43d4a1e650bc441b6357fe4ded8975b7151
Muhammad-Noval-Ansori <ancollann@gmail.com>
2026-07-22 04:10:37 +0800

    Update Barang.java

--- src/tugas/Barang.java
+++ src/tugas/Barang.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tugas;
 
-/**
- *
- * @author USER
- */
 public class Barang {
-    
+
+    private String nama;
+    private double harga;
+    private int stok;
+
+    public Barang(String nama, double harga, int stok) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public String keBaris() {
+        return nama + ";" + harga + ";" + stok;
+    }
+
+    public void info() {
+        System.out.println("Nama  : " + nama);
+        System.out.println("Harga : " + harga);
+        System.out.println("Stok  : " + stok);
+        System.out.println();
+    }
 }
